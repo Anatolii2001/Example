92c3004ef0e49668cba4ca5ae25e61bee2b7cf74
Анатолий <107363673+Anatolii2001@users.noreply.github.com>
2025-07-27 22:30:18 +0300

    Промежуточная аттестация Модуль 1 «Введение в разработку/Введение в Java»

---
+++ src/Аttestation_01/Person.java
@@ -0,0 +1,70 @@
+package Аttestation_01;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+class Person {
+    private final String name;
+    private final List<Product> products;
+    private double money;
+
+    public Person(String name, double money) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Имя не может быть пустым");
+        }
+        if (name.length() < 3) {
+            throw new IllegalArgumentException("Имя не может быть короче 3 символов");
+        }
+        if (money < 0) {
+            throw new IllegalArgumentException("Деньги не могут быть отрицательными");
+        }
+        this.name = name;
+        this.money = money;
+        this.products = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Метод покупки продукта
+    public boolean buyProduct(Product product) {
+        if (product == null) return false;
+        if (this.money >= product.getPrice()) {
+            this.money -= product.getPrice();
+            products.add(product);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Double.compare(money, person.money) == 0 && Objects.equals(name, person.name) && Objects.equals(products, person.products);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, money, products);
+    }
+
+    @Override
+    public String toString() {
+        if (products.isEmpty()) {
+            return name + " - Ничего не куплено";
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < products.size(); i++) {
+                sb.append(products.get(i).getName());
+                if (i < products.size() - 1) {
+                    sb.append(", ");
+                }
+            }
+            return name + " - " + sb;
+        }
+    }
+}
